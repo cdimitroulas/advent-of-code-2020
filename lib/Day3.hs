36772423fff683af485971c6ee939d8c53b1f243
cdimitroulas <c.dimitroulas@gmail.com>
2020-12-03 23:05:12 +0000

    Refactor out a createPath function

--- lib/Day3.hs
+++ lib/Day3.hs
@@ -9,6 +9,16 @@ accessCoordinate grid (x, y) = (grid !! y) !! x
 isTree :: Char -> Bool
 isTree x = x == '#'
 
+type Right = Int
+type Down = Int
+type Height = Int
+
+-- Given a number of steps right and a number of steps down + the height of the grid,
+-- returns a list of coordinates for the path through the grid
+-- e.g. createPath 3 1 = [(0, 0), (3, 1), (6, 2)...]
+createPath :: Right -> Down -> Height -> [Coordinate]
+createPath right down h = zip [0, right..] [0, down.. h]
+
 -- Given a grid and a list of coordinates, returns relevant the points
 getPoints :: [[Char]] -> [Coordinate] -> [Char]
 getPoints grid = filter isTree . map (accessCoordinate grid)
@@ -17,9 +27,8 @@ main :: IO ()
 main = do
   -- Read the file and repeat each line infinitely
   contents <- map cycle . lines <$> readFile "lib/day3.txt"
-  -- Create list of coordinates which we want to path through
-  -- e.g. [(0, 0), (3, 1), (6, 2) ...]
-  let path = zip [0, 3..] [0, 1.. length contents - 1]
+  let slopeHeight = length contents - 1
+  let path = createPath 3 1 slopeHeight
   let answer = length $ getPoints contents path
 
   putStrLn "Day 3 part 1:"
@@ -28,11 +37,11 @@ main = do
   putStrLn "Day 3 part 2:"
   -- Create a list of different paths
   let paths = [
-          zip [0, 1..] [0, 1.. length contents - 1],
+          createPath 1 1 slopeHeight,
           path,
-          zip [0, 5..] [0, 1.. length contents -1],
-          zip [0, 7..] [0, 1.. length contents -1],
-          zip [0, 1..] [0, 2.. length contents -1]
+          createPath 5 1 slopeHeight,
+          createPath 7 1 slopeHeight,
+          createPath 1 2 slopeHeight
         ]
   -- Calculate the number of trees hit for each path and use `product`
   -- to multiply all the numbers together
