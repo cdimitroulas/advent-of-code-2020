73dcf5e4c8479b4a1b18b5ec362b61d3e8dbe99c
cdimitroulas <c.dimitroulas@gmail.com>
2020-12-06 23:37:55 +0000

    Cleanup comments

--- lib/Day6.hs
+++ lib/Day6.hs
@@ -10,13 +10,13 @@ getNumberOfYesAnswers = sum . map (length . Set.fromList . concat . lines) . spl
 part1 :: IO ()
 part1 = readFile "lib/day6.txt" >>= print . getNumberOfYesAnswers
 
--- Given a list of strings like ["abc", "a", "b"] count the occurrences of each character
--- Create a Map with number of occurrences of each char. Then check
--- how many keys in the map are equal to the length of the list (indicated all strings in list
+-- Given a list of strings like ["abc", "a", "b"] count the occurrences of each character.
+-- 
+-- 1. Create a Map with number of occurrences of each char.
+-- 2. check how many keys in the map are equal to the length of the list (indicated all strings in list
 -- had that char)
 getNoOfQsEver1AnsweredYes :: [String] -> Int
 getNoOfQsEver1AnsweredYes list = Map.size . Map.filter (== length list). Map.unionsWith (+) . map (Map.fromSet (const (1 :: Int)) . Set.fromList) $ list
--- thing = foldl (\accum str -> Map.insertWith (++) 1 x) Map.empty
 
 getNumberOfYesAnswers' :: String -> Int
 getNumberOfYesAnswers' = sum . map (getNoOfQsEver1AnsweredYes . lines) . splitOn "\n\n"
