28560136c50c94d484520166271214d4e5b559a8
cdimitroulas <c.dimitroulas@gmail.com>
2020-12-09 22:03:24 +0000

    Day 7 part 1

---
+++ lib/Day7.hs
@@ -0,0 +1,69 @@
+module Day7 where
+
+import Data.List.Split
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+type Color = String
+
+data BagRule = EmptyBag Color | BagContaining Color [Color] deriving (Show, Eq)
+
+getColor :: BagRule -> Color
+getColor (EmptyBag c) = c
+getColor (BagContaining c _) = c
+
+-- Notes:
+
+-- * All colors are two words e.g. light red
+
+-- * First two words are always the colour of the bag that the rule describes
+
+-- * all contained bags follow the keyword "contain" and are separated by commas
+
+-- * an empty bag is denoted as containing "no other bags". Treat "other" as a special color?
+
+-- * the number of bags contained is rather meaningless for part1 so we can ignore them
+
+--
+
+strToBagRule :: String -> BagRule
+strToBagRule input = case head containedBagColors of
+  "other" -> EmptyBag bagColor
+  _ -> BagContaining bagColor containedBagColors
+  where
+    bagColor = (unwords . take 2 . words) input
+    -- Drop first 4 words which are always of form "light red bags contain"
+    -- Then chunk into groups of 4 words and take the middle two
+    -- since each group of 4 is always of the form "4 vivid green bags,"
+    containedBagColors = (map (unwords . init . tail) . chunksOf 4 . drop 4 . words) input
+
+strToBagRules :: String -> Map Color BagRule
+strToBagRules input =
+  Map.fromList
+    ( map
+        ( ( \x -> case x of
+              EmptyBag c -> (c, x)
+              BagContaining c _ -> (c, x)
+          )
+            . strToBagRule
+        )
+        $ lines input
+    )
+
+canContainGoldBag :: Map Color BagRule -> BagRule -> Bool
+canContainGoldBag _ (EmptyBag _) = False
+canContainGoldBag bagRuleMap (BagContaining _ containedColors) =
+  ("shiny gold" `elem` containedColors)
+    || any (canContainGoldBag bagRuleMap) bagRules
+  where
+    bagRules :: [BagRule]
+    bagRules = map (bagRuleMap Map.!) containedColors
+
+getNumberOfBagThatCanContainGold :: String -> Int
+getNumberOfBagThatCanContainGold input = (Map.size . Map.filter canContainGoldBag') bagRuleMap
+  where
+    bagRuleMap = strToBagRules input
+    canContainGoldBag' = canContainGoldBag bagRuleMap
+
+part1 :: IO ()
+part1 = readFile "lib/day7.txt" >>= print . getNumberOfBagThatCanContainGold
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,15 @@
-import Test.Hspec
-
 import qualified Day4 as D4
 import qualified Day5 as D5
 import qualified Day6 as D6
+import qualified Day7 as D7
+import Test.Hspec
 
 day3TestInput :: [[Char]]
-day3TestInput = [
-  "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
-  "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
-  "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
-  "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
+day3TestInput =
+  [ "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm",
+    "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884 hcl:#cfa07d byr:1929",
+    "hcl:#ae17e1 iyr:2013 eyr:2024 ecl:brn pid:760753108 byr:1931 hgt:179cm",
+    "hcl:#cfa07d eyr:2025 pid:166559648 iyr:2011 ecl:brn hgt:59in"
   ]
 
 main :: IO ()
@@ -34,3 +34,9 @@ main = hspec $ do
       it "returns the correct sum based on the example input" $ do
         input <- readFile "test/day6-example.txt"
         D6.getNumberOfYesAnswers' input `shouldBe` 6
+
+  describe "Day7 exercises" $ do
+    describe "Part1" $ do
+      it "returns the correct number of bags based on the example input" $ do
+        input <- readFile "test/day7-example.txt"
+        D7.getNumberOfBagThatCanContainGold input `shouldBe` 4
