5d1f19a694463c8829cc07698c3ae323e4a2d9a7
cdimitroulas <c.dimitroulas@gmail.com>
2020-12-13 17:32:45 +0000

    Organizationg

--- lib/Day8.hs
+++ lib/Day8.hs
@@ -1,7 +1,6 @@
 module Day8 where
 
 import Control.Monad.State
-import Debug.Trace
 import Text.Read
 
 newtype Direction = Direction Char deriving (Eq, Show)
@@ -11,12 +10,13 @@ mkDirection '+' = Just $ Direction '+'
 mkDirection '-' = Just $ Direction '-'
 mkDirection _ = Nothing
 
-getDirectionOperator :: Num a => Direction -> (a -> a -> a)
-getDirectionOperator dir = if unDirection dir == '+' then (+) else (-)
-
 unDirection :: Direction -> Char
 unDirection (Direction char) = char
 
+getDirectionOperator :: Num a => Direction -> (a -> a -> a)
+getDirectionOperator dir = if unDirection dir == '+' then (+) else (-)
+
+-- Data type for the different possible operations that the program can have
 data Command = Accum Direction Int | Jump Direction Int | Noop deriving (Eq, Show)
 
 mkCommand :: String -> Maybe Command
@@ -30,6 +30,10 @@ mkCommand str = do
     "nop" -> Just Noop
     _ -> Nothing
 
+-- Stateful things to track:
+--  - The accumulator value
+--  - The line number to execute next
+--  - Which lines have already been executed
 data ExecutionState = ExecutionState
   { progCommands :: [Command],
     progNextLineNo :: Int,
@@ -49,14 +53,9 @@ getNextAccum :: Int -> Command -> Int
 getNextAccum accum (Accum dir amt) = getDirectionOperator dir accum amt
 getNextAccum accum _ = accum
 
--- Stateful things to track:
---  - The accumulator value
---  - The line number to execute next
---  - Which lines have already been executed
---
 -- If we reach a line which has already been executed, then we want to return the accumulator
--- value.
-
+-- value. This is done by returning a Maybe from the stateful effect which is Nothing unless
+-- the next line to execute has already been executed previously.
 runNextLine :: State ExecutionState (Maybe Int)
 runNextLine = state $ \s ->
   let nextCommand = progCommands s !! progNextLineNo s
