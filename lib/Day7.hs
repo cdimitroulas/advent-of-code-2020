db0f010676cfeffef16ba4151a8ea6f9c418e17c
cdimitroulas <c.dimitroulas@gmail.com>
2020-12-13 13:26:31 +0000

    Complete Day7 part 2

--- lib/Day7.hs
+++ lib/Day7.hs
@@ -1,5 +1,6 @@
 module Day7 where
 
+import Data.List (foldl')
 import Data.List.Split
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -62,3 +63,44 @@ getNumberOfBagThatCanContainGold input = (Map.size . Map.filter canContainGoldBa
 
 part1 :: IO ()
 part1 = readFile "lib/day7.txt" >>= print . getNumberOfBagThatCanContainGold
+
+-- New version of BagRule which takes into account the number of bags
+data BagRule' = EmptyBag' Color | BagContaining' Color [(Int, Color)] deriving (Show, Eq)
+
+getColor' :: BagRule' -> Color
+getColor' (EmptyBag' c) = c
+getColor' (BagContaining' c _) = c
+
+getContainedBags :: BagRule' -> [(Int, Color)]
+getContainedBags (EmptyBag' _) = []
+getContainedBags (BagContaining' _ bags) = bags
+
+strToBagRule' :: String -> BagRule'
+strToBagRule' input = case head containedBagColors of
+  (_, "other") -> EmptyBag' bagColor
+  _ -> BagContaining' bagColor containedBagColors
+  where
+    bagColor = (unwords . take 2 . words) input
+    containedBagColors = (map (\x -> (read $ head x :: Int, (unwords . init . tail) x)) . chunksOf 4 . drop 4 . words) input
+
+-- Creates a Map of all the bag rules keyed by their color
+strToBagRules' :: String -> Map Color BagRule'
+strToBagRules' input = Map.fromList $ map (toKeyBagRuleTuple . strToBagRule') $ lines input
+  where
+    toKeyBagRuleTuple :: BagRule' -> (Color, BagRule')
+    toKeyBagRuleTuple rule = (getColor' rule, rule)
+
+countContainedBags :: Map Color BagRule' -> BagRule' -> Int
+countContainedBags _ (EmptyBag' _) = 0
+countContainedBags bagRuleMap bagRule =
+  foldl'
+    (+)
+    0
+    (map (\x -> fst x + fst x * countContainedBags bagRuleMap ((Map.!) bagRuleMap (snd x))) containedBagRules)
+  where
+    containedBagRules = getContainedBags bagRule
+
+part2 :: IO ()
+part2 = do
+  bagRules <- strToBagRules' <$> readFile "lib/day7.txt"
+  print $ countContainedBags bagRules <$> Map.lookup "shiny gold" bagRules
